16d574832998e325f64648ff22adc862e61a8b99
texx <hdtexx@gmail.com>
2026-03-20 20:48:48 +0100

    Copy console option in GD Tools Window for debug

--- Assets/Project/Scripts/Utils/Editor/GDToolsWindow.cs
+++ Assets/Project/Scripts/Utils/Editor/GDToolsWindow.cs
@@ -1,4 +1,6 @@
 #if UNITY_EDITOR
+using System;
+using System.Reflection;
 using Project.Scripts.Services.BoardEdit;
 using Project.Scripts.Tiles;
 using UnityEditor;
@@ -38,11 +40,58 @@ namespace Project.Scripts.Utils.Editor
             GUI.backgroundColor = prev;
 
             EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.Space(8);
+            EditorGUILayout.LabelField("Debug", EditorStyles.boldLabel);
+            EditorGUILayout.Space(4);
+            if (GUILayout.Button("Copy Console", GUILayout.Height(28)))
+                CopyConsole();
         }
 
         private void OnInspectorUpdate()
         {
             Repaint();
+        }
+
+
+        private void CopyConsole()
+        {
+            try
+            {
+                var logEntries = Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+                if (null == logEntries)
+                {
+                    Debug.LogError("Failed to access console types");
+                    return;
+                }
+
+                var getCountMethod = logEntries.GetMethod("GetCount", BindingFlags.Static | BindingFlags.Public);
+                var startGettingEntriesMethod = logEntries.GetMethod("StartGettingEntries", BindingFlags.Static | BindingFlags.Public);
+                var getEntryInternalMethod = logEntries.GetMethod("GetEntryInternal", BindingFlags.Static | BindingFlags.Public);
+                var endGettingEntriesMethod = logEntries.GetMethod("EndGettingEntries", BindingFlags.Static | BindingFlags.Public);
+
+                var count = (int)getCountMethod.Invoke(null, null);
+                var sb = new System.Text.StringBuilder();
+                startGettingEntriesMethod.Invoke(null, null);
+                var logEntryType = Type.GetType("UnityEditor.LogEntry, UnityEditor.dll");
+                var logEntry = Activator.CreateInstance(logEntryType);
+
+                for (var i = 0; i < count; i++)
+                {
+                    getEntryInternalMethod.Invoke(null, new[] { i, logEntry });
+                    var messageField = logEntryType.GetField("message", BindingFlags.Instance | BindingFlags.Public);
+                    var message = (string)messageField.GetValue(logEntry);
+                    var header = message.Split('\n')[0];
+                    sb.AppendLine(header);
+                }
+
+                endGettingEntriesMethod.Invoke(null, null);
+                GUIUtility.systemCopyBuffer = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Error while copying console:{ex.Message}");
+            }
         }
     }
 }
